91b20725a634c30375547dd4f988f9ed2c377e44
Can Zhang <can.zhang@blow-fire.com>
2024-05-29 17:22:36 +0800

    add note

--- image_utils.py
+++ image_utils.py
@@ -381,13 +381,13 @@ class MDImagineNote(Base):
     @classmethod
     def INPUT_TYPES(cls):
         note = """
-            image_index: 数字， 选择md 生成图像的第几张放大， md默认生成4张图， 默认选择第一张
-            aspect: 图像尺寸
-            chaos: 控制图片生成混乱度，越高图片越多样，越低图片变化越少, 默认0， 取值0-100
-            stylize: 调节图片艺术化程度，越高越艺术，越低越匹配， 默认500， 取值 0-1000
-            no:  强制去除图片中元素
-            niji: niji是一种动画风格模型, 是否启用该模型
-        """
+image_index: 数字， 选择md 生成图像的第几张放大， md默认生成4张图， 默认选择第一张
+aspect: 图像尺寸
+chaos: 控制图片生成混乱度，越高图片越多样，越低图片变化越少, 默认0， 取值0-100
+stylize: 调节图片艺术化程度，越高越艺术，越低越匹配， 默认500， 取值 0-1000
+no:  强制去除图片中元素
+niji: niji是一种动画风格模型, 是否启用该模型
+"""
         return {
             "required": {
                 "readme": ("STRING", {"default": note, "multiline": True}),
